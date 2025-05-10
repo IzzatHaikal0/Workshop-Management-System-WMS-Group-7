f0ee94f2447101749f969bbcac191803ddce59ad
NurulAinaAmran <nurulainaamran2002@gmail.com>
2025-05-10 15:20:23 +0800

    Save progress before swtiching branches

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:workshop_management_system/Screens/ManageForemanSchedule/ListSchedulePage.dart';
+import 'package:workshop_management_system/Screens/Profile/workshop_owner_profile_page.dart';
+import 'package:workshop_management_system/Screens/Profile/foreman_profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,31 +9,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Workshop Management Sytstem App',
+      title: 'Workshop Management System App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Workshop Management System App Home Page'),
+      home: const MyHomePage(title: 'Workshop Management System Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -46,10 +37,10 @@ class _MyHomePageState extends State<MyHomePage> {
   //navigation pages
   final List<Widget> _pages = [
     const Center(child: Text('Home Page')), // Home page
-    const SchedulePage(), // Navigate to SchedulePage
-    const Center(child: Text('Page 3')), // Placeholder for Page 3
-    const Center(child: Text('Page 4')), // Placeholder for Page 4
-    const Center(child: Text('Page 5')), // Placeholder for Page 5
+    const Center(child: Text('Page 2')), // Navigate to SchedulePage
+    const WorkshopOwnerProfilePage(), // Navigate to Workshop Owner Profile
+    const ForemanProfilePage(), // Navigate to Foreman Profile
+    const Center(child: Text('Settings')), // Placeholder for Settings
   ];
 
   void _onItemTapped(int index) {
@@ -65,15 +56,14 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.primary,
         title: Text(widget.title),
       ),
-      body: _pages[_selectedIndex], // Display the selected page
+      body: _pages[_selectedIndex], // Display selected page
 
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.blue,
         selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey, // Unselected item color
-        currentIndex:
-            _selectedIndex, // Set the current tab based on _selectedIndex
-        onTap: _onItemTapped, // Handle tab selection
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -84,16 +74,16 @@ class _MyHomePageState extends State<MyHomePage> {
             label: 'Schedule',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.access_alarm),
-            label: 'aina',
+            icon: Icon(Icons.home_repair_service),
+            label: 'Workshop Profile',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'aimar',
+            icon: Icon(Icons.engineering),
+            label: 'Foreman Profile',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
-            label: 'resya',
+            label: 'Settings',
           ),
         ],
       ),
