ed9c99fd976e33dd3d866f36f104ebcf2b6798b9
NurulAinaAmran <nurulainaamran2002@gmail.com>
2025-06-12 23:30:41 +0800

    Fix: remove incorrectly cased controllers and models folders

---
+++ lib/Models/user_model.dart
@@ -0,0 +1,17 @@
+class UserModel {
+  final String userRole;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String phoneNumber;
+  final String password;
+
+  UserModel({
+    required this.userRole,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.phoneNumber,
+    required this.password,
+  });
+}
